c023aa1762ff564254b7e78d21b9b0e5a885b9ea
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-11 10:23:52 -0400

    Create and project and finish setup

---
+++ java-zoos/src/test/java/tech/gebel/javaZoos/JavaZoosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.gebel.javaZoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaZoosApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
