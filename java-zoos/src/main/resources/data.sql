9e0b9a05c7a3ef1f8c5a855a0c791ff0b4b70c69
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-13 20:29:51 -0400

    Use proper snake case.

--- java-zoos/src/main/resources/data.sql
+++ java-zoos/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
 DELETE
-FROM zooanimals;
+FROM zoo_animals;
 
 DELETE
 FROM animals;
@@ -11,21 +11,21 @@ FROM telephones;
 DELETE
 FROM zoos;
 
-INSERT INTO zoos (zooid, zooname, created_by, created_date, last_modified_by, last_modified_date)
+INSERT INTO zoos (zoo_id, zoo_name, created_by, created_date, last_modified_by, last_modified_date)
          VALUES (1, 'Gladys Porter Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (2, 'Point Defiance Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (3, 'San Diego Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (4, 'San Antonio Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (5, 'Smithsonian National Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
-INSERT INTO telephones (phoneid, phonetype, phonenumber, zooid, created_by, created_date, last_modified_by, last_modified_date)
+INSERT INTO telephones (phone_id, phone_type, phone_number, zoo_id, created_by, created_date, last_modified_by, last_modified_date)
          VALUES (1, 'MAIN', '555-555-5555', 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (2, 'EDUCATION', '555-555-1234', 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (3, 'MEMBERSHIP', '555-555-4321', 1, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (4, 'MAIN', '123-555-5555', 4, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (5, 'MAIN', '555-123-5555', 3, 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
-INSERT INTO animals (animalid, animaltype, created_by, created_date, last_modified_by, last_modified_date)
+INSERT INTO animals (animal_id, animal_type, created_by, created_date, last_modified_by, last_modified_date)
          VALUES (1, 'lion', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (2, 'bear', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (3, 'monkey', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
@@ -34,7 +34,7 @@ INSERT INTO animals (animalid, animaltype, created_by, created_date, last_modifi
                 (6, 'llama',  'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (7, 'turtle', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP);
 
-INSERT INTO zooanimals (zooid, animalid, incomingzoo, created_by, created_date, last_modified_by, last_modified_date)
+INSERT INTO zoo_animals (zoo_id, animal_id, incoming_zoo, created_by, created_date, last_modified_by, last_modified_date)
          VALUES (1, 1, 'Point Defiance Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (2, 2, 'Gladys Porter Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
                 (1, 2, 'Point Defiance Zoo', 'SYSTEM', CURRENT_TIMESTAMP, 'SYSTEM', CURRENT_TIMESTAMP),
