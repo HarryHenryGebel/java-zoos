85ffb3d284af7255c84101c60b5d414f197d12ca
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-14 14:53:58 -0400

    Make bean from implementation instead of interface

--- java-zoos/src/main/java/tech/gebel/javazoos/services/AnimalServices.java
+++ java-zoos/src/main/java/tech/gebel/javazoos/services/AnimalServices.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import tech.gebel.javazoos.views.AnimalZooCount;
 
-@Service
 public interface AnimalServices {
   List<AnimalZooCount> getAnimalZooCounts();
 }
--- java-zoos/src/main/java/tech/gebel/javazoos/services/AnimalServicesImplementation.java
+++ java-zoos/src/main/java/tech/gebel/javazoos/services/AnimalServicesImplementation.java
@@ -1,9 +1,11 @@
 package tech.gebel.javazoos.services;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
 import tech.gebel.javazoos.repositories.AnimalRepository;
 import tech.gebel.javazoos.views.AnimalZooCount;
 
+@Service
 public class AnimalServicesImplementation implements AnimalServices {
   private final AnimalRepository animalRepository;
 
